e0e2340b51db4a59eeba9cb0355ed585f5e0864b
Cesar Acosta <c3sar.adan@gmail.com>
2024-02-03 13:38:12 -0700

    Agregar archivos de proyecto.

---
+++ Paint1014/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Paint1014
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picBoxPapel = new PictureBox();
+            btnBorrador = new Button();
+            btnPinncel = new Button();
+            trackBarTamnioPincel = new TrackBar();
+            PicBoxNegro = new PictureBox();
+            picBoxColores = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtR = new TextBox();
+            txtG = new TextBox();
+            txtB = new TextBox();
+            colorDialog1 = new ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)picBoxPapel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarTamnioPincel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PicBoxNegro).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picBoxColores).BeginInit();
+            SuspendLayout();
+            // 
+            // picBoxPapel
+            // 
+            picBoxPapel.BackColor = Color.White;
+            picBoxPapel.Location = new Point(168, 12);
+            picBoxPapel.Name = "picBoxPapel";
+            picBoxPapel.Size = new Size(1166, 632);
+            picBoxPapel.TabIndex = 0;
+            picBoxPapel.TabStop = false;
+            picBoxPapel.MouseDown += picBoxPapel_MouseDown;
+            picBoxPapel.MouseMove += picBoxPapel_MouseMove;
+            picBoxPapel.MouseUp += picBoxPapel_MouseUp;
+            // 
+            // btnBorrador
+            // 
+            btnBorrador.Location = new Point(12, 12);
+            btnBorrador.Name = "btnBorrador";
+            btnBorrador.Size = new Size(94, 29);
+            btnBorrador.TabIndex = 1;
+            btnBorrador.Text = "Borrador";
+            btnBorrador.UseVisualStyleBackColor = true;
+            btnBorrador.Click += btnBorrador_Click;
+            // 
+            // btnPinncel
+            // 
+            btnPinncel.Location = new Point(12, 47);
+            btnPinncel.Name = "btnPinncel";
+            btnPinncel.Size = new Size(94, 29);
+            btnPinncel.TabIndex = 2;
+            btnPinncel.Text = "Pincel";
+            btnPinncel.UseVisualStyleBackColor = true;
+            btnPinncel.Click += btnPinncel_Click;
+            // 
+            // trackBarTamnioPincel
+            // 
+            trackBarTamnioPincel.Location = new Point(12, 92);
+            trackBarTamnioPincel.Maximum = 20;
+            trackBarTamnioPincel.Minimum = 1;
+            trackBarTamnioPincel.Name = "trackBarTamnioPincel";
+            trackBarTamnioPincel.Orientation = Orientation.Vertical;
+            trackBarTamnioPincel.Size = new Size(56, 212);
+            trackBarTamnioPincel.TabIndex = 3;
+            trackBarTamnioPincel.Value = 3;
+            // 
+            // PicBoxNegro
+            // 
+            PicBoxNegro.BackColor = Color.Black;
+            PicBoxNegro.Location = new Point(12, 310);
+            PicBoxNegro.Name = "PicBoxNegro";
+            PicBoxNegro.Size = new Size(26, 29);
+            PicBoxNegro.TabIndex = 4;
+            PicBoxNegro.TabStop = false;
+            PicBoxNegro.Click += PicBoxNegro_Click;
+            // 
+            // picBoxColores
+            // 
+            picBoxColores.BackColor = Color.IndianRed;
+            picBoxColores.Location = new Point(44, 310);
+            picBoxColores.Name = "picBoxColores";
+            picBoxColores.Size = new Size(26, 29);
+            picBoxColores.TabIndex = 5;
+            picBoxColores.TabStop = false;
+            picBoxColores.Click += picBoxColores_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 353);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 20);
+            label1.TabIndex = 6;
+            label1.Text = "R";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 383);
+            label2.Name = "label2";
+            label2.Size = new Size(19, 20);
+            label2.TabIndex = 7;
+            label2.Text = "G";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 416);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 20);
+            label3.TabIndex = 8;
+            label3.Text = "B";
+            // 
+            // txtR
+            // 
+            txtR.Location = new Point(36, 350);
+            txtR.Name = "txtR";
+            txtR.Size = new Size(59, 27);
+            txtR.TabIndex = 9;
+            txtR.Text = "0";
+            // 
+            // txtG
+            // 
+            txtG.Location = new Point(36, 383);
+            txtG.Name = "txtG";
+            txtG.Size = new Size(59, 27);
+            txtG.TabIndex = 10;
+            txtG.Text = "0";
+            // 
+            // txtB
+            // 
+            txtB.Location = new Point(36, 416);
+            txtB.Name = "txtB";
+            txtB.Size = new Size(59, 27);
+            txtB.TabIndex = 11;
+            txtB.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1346, 656);
+            Controls.Add(txtB);
+            Controls.Add(txtG);
+            Controls.Add(txtR);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(picBoxColores);
+            Controls.Add(PicBoxNegro);
+            Controls.Add(trackBarTamnioPincel);
+            Controls.Add(btnPinncel);
+            Controls.Add(btnBorrador);
+            Controls.Add(picBoxPapel);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)picBoxPapel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarTamnioPincel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PicBoxNegro).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picBoxColores).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picBoxPapel;
+        private Button btnBorrador;
+        private Button btnPinncel;
+        private TrackBar trackBarTamnioPincel;
+        private PictureBox PicBoxNegro;
+        private PictureBox picBoxColores;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtR;
+        private TextBox txtG;
+        private TextBox txtB;
+        private ColorDialog colorDialog1;
+    }
+}
---
+++ Paint1014/Form1.cs
@@ -0,0 +1,99 @@
+namespace Paint1014
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        int x = 0;
+        int y = 0;
+        int R = 0;
+        int G = 0;
+        int B = 0;
+        int tamnioPincel = 3;
+        bool moviendo = false;
+        Pen pen;
+        bool pintar = false;
+        bool borrar = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            picBoxPapel.Image = new Bitmap(picBoxPapel.Height, picBoxPapel.Width);
+            papel = picBoxPapel.CreateGraphics();
+            papel.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            tamnioPincel = trackBarTamnioPincel.Value;
+            R = int.Parse(txtR.Text);
+            G = int.Parse(txtG.Text);
+            B = int.Parse(txtB.Text);
+            pen = new Pen(Color.FromArgb(R, G, B), tamnioPincel);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+        }
+
+        private void btnPinncel_Click(object sender, EventArgs e)
+        {
+            pintar = true;
+            borrar = false;
+        }
+
+        private void picBoxPapel_MouseDown(object sender, MouseEventArgs e)
+        {
+            moviendo = true;
+            x = e.X;
+            y = e.Y;
+            picBoxPapel.Cursor = Cursors.Cross;
+        }
+
+        private void picBoxPapel_MouseUp(object sender, MouseEventArgs e)
+        {
+            moviendo = false;
+        }
+
+        private void btnBorrador_Click(object sender, EventArgs e)
+        {
+            pintar = false;
+            borrar = true;
+        }
+
+        private void picBoxPapel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moviendo && pintar)
+            {
+                cambiarPincel(int.Parse(txtR.Text), int.Parse(txtG.Text), int.Parse(txtB.Text));
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+            if (moviendo && borrar)
+            {
+                cambiarPincel(255, 255, 255);
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void cambiarPincel(int R, int G, int B)
+        {
+            pen = new Pen(Color.FromArgb(R, G, B), trackBarTamnioPincel.Value);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+        }
+
+        private void PicBoxNegro_Click(object sender, EventArgs e)
+        {
+            txtR.Text = 0.ToString();
+            txtG.Text = 0.ToString();
+            txtB.Text = 0.ToString();
+        }
+
+        private void picBoxColores_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtR.Text = colorDialog1.Color.R.ToString();
+                txtG.Text = colorDialog1.Color.G.ToString();
+                txtB.Text = colorDialog1.Color.B.ToString();
+            }
+        }
+    }
+}
